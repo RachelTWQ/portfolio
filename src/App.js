048e1cad9b659c90676a7e6263e79168294d4b88
RachelTWQ <racheltang15@hotmail.com>
2019-06-18 00:31:06 -0400

    add nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './home.js';
+import Nav from './nav.js';
 import './App.css';
 
 function App() {
   return (
     <Router>
+      <Nav />
       <Switch>
         <Route exact path='/' component={() => <Home />}/>
         <Route path='/projects' />
