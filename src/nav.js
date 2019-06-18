25019bc472e981e7a5fd9df4a6219cd315bf163a
RachelTWQ <racheltang15@hotmail.com>
2019-06-18 12:20:48 -0400

    add links to nav

---
+++ src/nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+export default function Nav() {
+    return (
+        <div className='nav'>
+            <a href='https://resume.creddle.io/resume/e0p2fjjet2f'>Resume</a>
+            <Link to='/projects'>Projects</Link>
+            <Link to='/contact'>Contact</Link>
+        </div>
+    )
+}
