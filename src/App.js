e6ece105123f850fb6a3e1afcaaa8ad8ff68d860
RachelTWQ <racheltang15@hotmail.com>
2019-06-21 17:50:26 -0400

    add projects component to router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './home.js';
 import Nav from './nav.js';
 import Contact from './contact.js';
+import Projects from './projects.js';
 import './App.css';
+
+// const container = (WrappedComponent) => {
+//   class Container extends React.Component {
+//     render() {
+//       return (
+//         <WrappedComponent />
+//       )
+//     }
+//   }
+//   return Container;
+// }
 
 function App() {
   return (
@@ -11,7 +23,7 @@ function App() {
       <Nav />
       <Switch>
         <Route exact path='/' component={() => <Home />}/>
-        <Route path='/projects' />
+        <Route path='/projects' component={() => <Projects />}/>
         <Route path='/contact' component={() => <Contact />}/>
       </Switch>
     </Router>
