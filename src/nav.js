d351cdf89772500278c4405a9967de5316d3c3a9
RachelTWQ <racheltang15@hotmail.com>
2019-06-21 17:50:04 -0400

    add feature of open resume on new tab

--- src/nav.js
+++ src/nav.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
     return (
-        <div className='nav'>
+        <div className="nav">
             <Link to='/'>Rachel Tang</Link>
-            <a href='https://resume.creddle.io/resume/e0p2fjjet2f'>Resume</a>
-            <Link to='/projects'>Projects</Link>
-            <Link to='/contact'>Contact</Link>
+            <a href="https://resume.creddle.io/resume/e0p2fjjet2f" target="_blank" rel="noopener noreferrer">Resume</a>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
         </div>
     )
 }
