87de9511a0e7068a6b68b6a046ce0f03369c8c3b
RachelTWQ <racheltang15@hotmail.com>
2019-06-24 18:59:27 -0400

    hide white space from nav bar

--- src/nav.js
+++ src/nav.js
@@ -4,33 +4,33 @@ import './nav.css';
 
 export default function Nav() {
   return (
+    <>
+    <input id="main-menu-checkbox" type="checkbox" />
     <header>
       <div>
-      <a href="#main-menu" id="main-menu-toggle" className="menu-toggle" aria-label="Open main menu">
+      <label htmlFor="main-menu-checkbox" className="menu-toggle">
         <span className="sr-only">Open main menu</span>
         <span className="fa fa-bars" aria-hidden="true"></span>
-      </a>
+      </label>
       <Link to='/' className="logo">Rachel Tang</Link>
       </div>
-      <nav id="main-menu" className="main-menu" aria-label="Main menu">
-        <a href="#main-menu-toggle"
-          id="main-menu-close"
-          className="menu-close"
-          aria-label="Close main menu">
-          <span className="sr-only">Close main menu</span>
-          <span className="fa fa-close" aria-hidden="true"></span>
-        </a> 
+      <nav role="navigation" id="main-menu" className="main-menu" aria-expanded="false" aria-label="Main menu">
+      <label htmlFor="main-menu-checkbox" className="menu-close">
+        <span className="sr-only">Close main menu</span>
+        <span className="fa fa-times" aria-hidden="true"></span>
+      </label>
         <ul>
           <li><a href="https://resume.creddle.io/resume/e0p2fjjet2f" target="_blank" rel="noopener noreferrer">Resume</a></li>
           <li><Link to="/projects">Projects</Link></li>
           <li><Link to="/contact">Say Hi</Link></li>
-        </ul> 
+        </ul>
       </nav>
-      <a href="#main-menu-toggle"
+      <label htmlFor="main-menu-checkbox"
         className="backdrop"
         tabIndex="-1"
         aria-hidden="true" hidden>
-      </a>
-    </header>  
+      </label>
+    </header> 
+    </> 
   )
 }
