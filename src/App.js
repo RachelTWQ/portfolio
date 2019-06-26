cfd4e48ecb40939d3e0290ac97147e4c4ab6da19
RachelTWQ <racheltang15@hotmail.com>
2019-06-26 16:53:41 -0400

    add particle.js background

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from './home.js';
 import Nav from './nav.js';
 import Contact from './contact.js';
 import Projects from './projects.js';
+
+import Particles from 'react-particles-js';
 import './App.css';
 
 // const container = (WrappedComponent) => {
@@ -16,15 +18,67 @@ import './App.css';
 //   }
 //   return Container;
 // }
+const particleParams = {
+  "particles": {
+    "number": {
+      "value": 160,
+      "density": {
+        "enable": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "speed": 4,
+        "size_min": 0.3
+      }
+    },
+    "line_linked": {
+      "enable": false
+    },
+    "move": {
+      "random": true,
+      "speed": 1,
+      "direction": "top",
+      "out_mode": "out"
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "repulse"
+      }
+    },
+    "modes": {
+      "bubble": {
+        "distance": 250,
+        "duration": 2,
+        "size": 0,
+        "opacity": 0
+      },
+      "repulse": {
+        "distance": 400,
+        "duration": 4
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <Router>
+      <Particles className="particles" params={particleParams} />
       <Nav />
       <Switch>
-        <Route exact path='/' component={() => <Home />}/>
-        <Route path='/projects' component={() => <Projects />}/>
-        <Route path='/contact' component={() => <Contact />}/>
+        <Route exact path='/' component={() => <Home />} />
+        <Route path='/projects' component={() => <Projects />} />
+        <Route path='/contact' component={() => <Contact />} />
       </Switch>
     </Router>
   );
