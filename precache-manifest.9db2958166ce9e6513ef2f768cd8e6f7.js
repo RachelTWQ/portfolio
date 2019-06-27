2404f686bae56cedbabf484646555ac330e385d0
RachelTWQ <racheltang15@hotmail.com>
2019-06-27 13:07:08 -0400

    Updates

---
+++ precache-manifest.9db2958166ce9e6513ef2f768cd8e6f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b70d922291b99fd812daad424961c667",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a18a92aee28a03fa9d8f",
+    "url": "/portfolio/static/css/main.42d8721c.chunk.css"
+  },
+  {
+    "revision": "bba493db8c270082ada1",
+    "url": "/portfolio/static/js/2.33182b9c.chunk.js"
+  },
+  {
+    "revision": "a18a92aee28a03fa9d8f",
+    "url": "/portfolio/static/js/main.aeec0dbd.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "6ee5a7a80054224c3aa082bd8a3eb942",
+    "url": "/portfolio/static/media/Rachel.6ee5a7a8.jpg"
+  }
+]);
